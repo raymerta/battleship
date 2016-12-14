d7fae428c2168e954b02cec0004876a9670f0b8c
Radita Liem <hesti@ut.ee>
2016-12-14 07:41:58 +0700

    dynamically create new game room

--- server.py
+++ server.py
@@ -269,21 +269,34 @@ gameRoomName = ['Atlantic Ocean', 'Arctic Ocean', 'Indian Ocean', 'Pacific Ocean
 def createGameRoom(tiles, player, serverId, username): 
 	content = getAllSessions()
 
+	# generate random game room name
+	roomType = random.randint(0, (len(gameRoomName) - 1))
+	roomName = gameRoomName[roomType]
+
 	if (content != ""):
 		sessions = json.loads(content)
-		#todo
+		roomId = len(sessions) + 1
 
-	else:
-		#empty json file, proceed to create initial content
-		roomType = random.randint(0, (len(gameRoomName) - 1))
-		roomName = gameRoomName[roomType]
+		data = {"serverId" : serverId, "numPlayers" : player, "size" : tiles, "creator": username , "roomName": roomName, "roomCreated": int(time.time()), "isPlaying": False, "isEnded" : False , "gameRoomId" : roomId, "users": [{"username": username, "isTurn" : False, "isDefeated" : False, "isWinning" : False }] }
+		sessions.append(data)
 
-		data = [{"serverId" : serverId, "numPlayers" : player, "size" : tiles, "creator": username , "roomName": roomName, "roomCreated": int(time.time()), "isPlaying": False, "isEnded" : False , "gameRoomId" : 1, "users": [{"username": username, "isTurn" : False, "isDefeated" : False, "isWinning" : False }] }]
-		content = json.dumps(data)
+		dataObject = json.dumps(sessions)
 
 		fsource = 'sessions.json'
 		f = open(fsource, 'w')
-		f.write(content)
+		f.write(dataObject)
+		f.close()
+
+		return roomId
+
+	else:
+		#empty json file, proceed to create initial content
+		data = [{"serverId" : serverId, "numPlayers" : player, "size" : tiles, "creator": username , "roomName": roomName, "roomCreated": int(time.time()), "isPlaying": False, "isEnded" : False , "gameRoomId" : 1, "users": [{"username": username, "isTurn" : False, "isDefeated" : False, "isWinning" : False }] }]
+		dataObject = json.dumps(data)
+
+		fsource = 'sessions.json'
+		f = open(fsource, 'w')
+		f.write(dataObject)
 		f.close()
 
 		#return user ID number
