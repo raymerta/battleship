dbb9e65c2255d58eadc1b3e108dcd7f6b94e733a
Radita Liem <hesti@ut.ee>
2016-12-15 17:18:21 +0700

    correcting the listener

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import pika
 import common
 import json
 
+
 #get list of servers for pushing content
 def getMQGamesList():
 	games = []
@@ -11,6 +12,15 @@ def getMQGamesList():
 
 	for server in servers: 
 		games.append("game-%s" % server['serverUrl'])
+
+	return games
+
+def getMQServerList():
+	games = []
+	servers = json.loads(common.getAllServers())
+
+	for server in servers: 
+		games.append("server-%s" % server['serverUrl'])
 
 	return games
 
@@ -26,6 +36,9 @@ def updateSession():
 
 	return sessions
 
+serverContent = ''
+sessionContent = updateSession()
+
 # rabbitmq credentials
 mq_creds  = pika.PlainCredentials(username = "guest", password = "guest")
 
@@ -36,6 +49,7 @@ mq_params = pika.ConnectionParameters(host = "localhost", credentials = mq_creds
 mq_exchange = "amq.topic"
 mq_servers = "servers"
 mq_games = getMQGamesList()
+mq_serverListener = getMQServerList()
 mq_queue = "sessions"
 
 # connection object
@@ -51,8 +65,45 @@ for key in mq_games:
 		queue = mq_queue,
    		routing_key = key)
 
+for key in mq_serverListener:
+	mq_chan.queue_bind(
+		exchange = mq_exchange,
+		queue = mq_queue,
+   		routing_key = key)
+
 def callback(ch, method, properties, body):
     print("%r:%r" % (method.routing_key, body))
+    destination = method.routing_key.split("-")[0]
+
+    sessions = updateSession()
+
+    for key in mq_games: 
+    	if (sessions[key] != sessionContent[key]):
+    		print key
+    		sessionContent[key] = sessions[key]
+    		mq_chan.basic_publish(
+				exchange    = mq_exchange,
+				routing_key = key,
+    			body        = sessionContent[key])
+
+    ## TODO REFACTOR ALL REDUNDANT HTTP POST
+
+  #   if (destination == "server"):
+  #   	username = body.strip().split(";")[0]
+		# serverId = body.strip().split(";")[1]
+		# serverUrl = body.strip().split(";")[2]
+		# tiles = body.strip().split(";")[3]
+		# player = body.strip().split(";")[4]
+
+		# gameCreated = common.createGameRoom(tiles, player, serverId, username)
+
+		# if (gameCreated > 0):
+		# 	content = 'http://localhost:10001/game/%s/%s/%s' % (serverUrl, username, gameCreated)
+		# 	mq_chan.basic_publish(
+		# 	exchange    = mq_exchange,
+		# 	routing_key = mq_servers,
+  #   		body        = serverContent)
+
 
 mq_chan.basic_consume(callback,
         queue=mq_queue,
@@ -60,33 +111,6 @@ mq_chan.basic_consume(callback,
 
 mq_chan.start_consuming()
 
-serverContent = ''
-sessionContent = updateSession()
 
-# while True:
-# 	servers = common.getAllServers()
-
-# 	if (serverContent != servers):
-# 		print "new content"
-
-# 		serverContent = servers
-# 		mq_games = getMQGamesList()
-
-# 		mq_chan.basic_publish(
-# 			exchange    = mq_exchange,
-# 			routing_key = mq_servers,
-#     		body        = serverContent)
-
-# 	#stupid way to update session
-# 	sessions = updateSession()
-# 	for key in mq_games: 
-# 		if (sessions[key] != sessionContent[key]):
-# 			print key
-
-# 			sessionContent[key] = sessions[key]
-# 			mq_chan.basic_publish(
-# 				exchange    = mq_exchange,
-# 				routing_key = key,
-#     			body        = sessionContent[key])
 
 
